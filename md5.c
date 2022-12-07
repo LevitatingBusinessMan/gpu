9a5efc17c73f6927fd1b670b95f506e418bd447c
LevitatingBusinessMan <public@reinfernhout.xyz>
2022-12-07 04:34:52 +0100

    working md5

--- md5.c
+++ md5.c
@@ -1,39 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
 /* Basic MD5 functions */
 #define F(x, y, z) ((x & y) | (~x & z))
 #define G(x, y, z) ((x & z) | (y & ~z))
 #define H(x, y, z) (x ^ y ^ z)
 #define I(x, y, z) (y ^ (x | ~z))
 
-/* ROTATE_LEFT rotates x left n bits */
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+#define ROTATE_LEFT(x, n) (x << n | x >> 32-n)
 
 // message is 512 bits (should be 16 ints)
 // padded like message + 1 + many zeros + 64bit-length
-void md5 (char* s, int* k, char* message, int* result) {
-	int A, a0;
+void md5 (uint32_t* s, uint32_t* k, uint32_t* message, uint32_t* digest) {
+	uint32_t A, a0;
 	A = a0 = 0x67452301;
-	int B, b0;
+	uint32_t B, b0;
 	B = b0 = 0xefcdab89;
-	int C, c0;
+	uint32_t C, c0;
 	C = c0 = 0x98badcfe;
-	int D, d0;
+	uint32_t D, d0;
 	D = d0 = 0x10325476;
 
-	for (int i=0; i < 64; i++) {
-		int f, g;
-		if (0 <= i <= 15) {
+	for (uint32_t i=0; i < 64; i++) {
+		uint32_t f, g;
+		if (i < 16) {
 			f = F(B, C, D);
 			g = i; 
 		}
-		else if (16 <= i <= 31) {
+		else if (i < 32) {
 			f = G(B, C, D);
 			g = (5 * i + 1) % 16;
 		}
-		else if (32 <= i <= 47) {
+		else if (i < 48) {
 			f = H(B, C, D);
 			g = (3 * i + 5) % 16;
 		}
-		else if (48 <= i <= 63) {
+		else if (i < 64) {
 			f = I(B, C, D);
 			g = (7 * i) % 16;
 		}
@@ -44,11 +47,21 @@ void md5 (char* s, int* k, char* message, int* result) {
 		B = B + ROTATE_LEFT(f, s[i]);
 	}
 
+	digest[0] = a0 + A;
+	digest[1] = b0 + B;
+	digest[2] = c0 + C;
+	digest[3] = d0 + D;
+}
 
-	result[0] = a0 + A;
-	result[1] = b0 + B;
-	result[2] = c0 + C;
-	result[3] = d0 + D;
+uint32_t le(uint32_t integer) {
+	uint8_t bytes[4];
+	bytes[0] = (integer >> 24) & 0xff;
+	bytes[1] = (integer >> 16) & 0xff;
+	bytes[2] = (integer >> 8)  & 0xff;
+	bytes[3] =  integer        & 0xff; 
+	uint32_t result;
+	memcpy(&result, bytes, sizeof(uint32_t));
+	return result;
 }
 
 void main() {
@@ -74,13 +87,18 @@ void main() {
 	};
 
 	int message[] = {
-		0xb00bb00b, 0xb00bb00b, 0xb00bb00b, 0xb00bb00c,
+		0xb00bb00b, 0xb00bb00b, 0xb00bb00b, 0xb00bb00b,
+		0x00000080, 0x00000000, 0x00000000, 0x00000000, // append a bit, meaning append 0x80 byte
 		0x00000000, 0x00000000, 0x00000000, 0x00000000,
-		0x00000000, 0x00000000, 0x00000000, 0x00000000,
-		0x00000000, 0x00000000, 0x00000000, 0x00000020,
+		0x00000000, 0x00000000, 0x00000080, 0x00000000, // last 64 bits is length in bits
 	};
 
-	int result[4] = {0};
+	int digest[4] = {0};
 
-	md5(s,k,message,result);
+	md5(s,k,message,digest);
+
+	// Make little endian
+	for (int i=0; i<4; i++) digest[i] = le(digest[i]);
+
+	printf("%08x%08x%08x%08x\n",digest[0],digest[1],digest[2],digest[3]);
 }
